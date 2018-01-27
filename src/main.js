8971e7d7b2790cfae9ad73036da8b3e0f387428a
beatorizu <betrizaf@gmail.com>
2018-01-27 16:25:04 -0200

    Add Husky

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const a = 3;
+const b = 2;
+
+const c = 'dsfknkfrgvesgerrrrrrrrrrrrrrrrrrrrrrrrgwaqmwmwfeeeeweçfeeeeeeeeeeeeeeeeeeeeeeeeeeewwwwwwwwwwwf';
+
+function sum() {
+  return a + b + c
+}
+
+sum();
